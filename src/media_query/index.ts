1b0dd0e88e4afdda078c92f5643109e47d9001d1
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-02-22 15:04:20 +0300

    feat(media_query): new function

---
+++ src/media_query/index.ts
@@ -0,0 +1,34 @@
+import { to_readable, to_writable, unstore } from "@sveu/shared"
+
+import { on } from "../event_listener"
+import { support } from "../support"
+
+/**
+ * Reactive Media Query.
+ *
+ * @param query - Media Query
+ *
+ * @returns Readable Store
+ *
+ */
+export function media_query(query: string) {
+	if (!window) return to_readable(false)
+
+	const supported = support("matchMedia", "window")
+
+	if (!unstore(supported)) return to_readable(false)
+
+	const { subscribe, set } = to_writable(false)
+
+	const media_query: MediaQueryList | undefined = window.matchMedia(query)
+
+	function handler(event: MediaQueryListEvent) {
+		set(event.matches)
+	}
+
+	set(media_query.matches)
+
+	on(media_query, "change", handler)
+
+	return { subscribe }
+}
