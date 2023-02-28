2f0024ed24fcef0ee7c4149ba24f338b242c75e9
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-02-28 11:12:47 +0300

    feat(window_scroll): new function

---
+++ src/window_scroll/index.ts
@@ -0,0 +1,33 @@
+import { to_readable, to_writable } from "@sveu/shared"
+
+import { on } from "../event_listener"
+
+/**
+ * Reactive window scroll.
+ *
+ * @returns
+ * - `x`: A readable store with the current window scroll x position.
+ * - `y`: A readable store with the current window scroll y position.
+ */
+export function window_scroll() {
+	if (!window) return { x: to_readable(0), y: to_readable(0) }
+
+	const x = to_writable(window.scrollX)
+
+	const y = to_writable(window.scrollY)
+
+	on(
+		window,
+		"scroll",
+		() => {
+			x.set(window.scrollX)
+			y.set(window.scrollY)
+		},
+		{
+			capture: false,
+			passive: true,
+		}
+	)
+
+	return { x: to_readable(x), y: to_readable(y) }
+}
